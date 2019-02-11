8efe02bd7ebe3cb51cea2913dfe8d34327d5396b
bismayswain <bismayswain@gmail.com>
2019-02-11 18:20:47 +0530

    Change directory structure

---
+++ assignment_2/tests/input1/test1.go
@@ -0,0 +1,13 @@
+// Our first program will print the classic "hello world"
+// message. Here's the full source code.
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("hello world");
+}
+σΣς
+σίγμα
+
+// Comment
