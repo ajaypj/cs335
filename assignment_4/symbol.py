6f23cdfd0ae449483cf1ff76e83b9aa3452a5b41
siddhantkar <siddhk@iitk.ac.in>
2019-04-05 04:26:18 +0530

    Milestone 3

---
+++ assignment_4/symbol.py
@@ -0,0 +1,33 @@
+class symbolTable:
+
+    def __init__(self, par=None):
+        self.table = {}
+        self.parent = par
+        self.mem = 0
+
+    # Checks whether "name" lies in the symbol table
+    def lookUp(self, name):
+        return (name in self.table)
+
+    # Inserts if already not present
+    def insert(self, name, type):
+        if (not self.lookUp(name)):
+            (self.table)[name] = {}
+            (self.table)[name]['type'] = type
+
+    # Returns the argument list of the variable else returns None
+    # Note that type is always a key in argument list
+    def getInfo(self, name):
+        if(self.lookUp(name)):
+            return (self.table)[name]
+        return None
+
+    # Updates the variable of NAME name with arg list of KEY key with VALUE value
+    def update(self, name, key, value):
+        if (self.lookUp(name)):
+            (self.table)[name][key] = value
+        else:
+            raise KeyError("Symbol " + name + " doesn't exist, cant update")
+
+    def setParent(self, parent):
+        self.parent = parent
