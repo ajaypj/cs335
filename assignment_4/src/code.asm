2d53e09dbcd2b6869c34b5522cb847c2808cc752
siddhantkar <siddhk@iitk.ac.in>
2019-04-15 17:38:30 +0530

    mul

--- assignment_4/src/code.asm
+++ assignment_4/src/code.asm
@@ -5,10 +5,13 @@ sub $8, %esp
 push %ebx
 push %esi
 push %edi
-mov $2, %eax
+mov -4(%ebp), %eax
 imul $5, %eax
 mov $4, %ecx
 sub %eax, %ecx
+mov %ecx, -8(%ebp)
+mov -8(%ebp), %eax
+mov %eax, -4(%ebp)
 sub $0, %esp
 pop %edi
 pop %esi
--- assignment_4/src/gen.py
+++ assignment_4/src/gen.py
@@ -101,7 +101,7 @@ file.close()
 op_dic = {'+':"add", '-':"sub", '*':"imul", '/':"idiv"}
 
 for line in ir:
-	data = re.split(" |,|\n", line)
+	data = re.split(" |, |\n", line)
 	i = list()
 	for w in data:
 		if w:
@@ -111,35 +111,87 @@ for line in ir:
 	# print(regs)
 	# print(temp_vars)
 	# print(unused_temp_vars_in_reg)
-	if i[0] ==  "funcstart":
+	if i[0] == "funcstart":
 		function_width = int(i[1])
 		code += [i[2]]
 
-	if i[0] ==  "freetmp":
+	if i[0] == "freetmp":
 		code += ["sub ${}, %esp".format(temp_offset)]
 
-	elif i[0] in ["push", "pop", "call", "mov", "add", "sub"]: # push and pop
+	if i[0] == "retval":
+		if get_type(i[1]) == "c":
+			code += ["mov {}, %eax".format(i[1])]
+		elif get_type(i[1]) == "m":
+			offset = 0
+			if "var" in i[1]:
+				offset = int(split_var(i[1])[1])
+			elif "tmp#" in i[1]:
+				temp_var_name = split_var(i[1])[0]
+				offset = temp_vars[temp_var_name]["so"]
+			code += ["mov {}(%ebp), %eax".format(-offset)]
+		elif get_type(i[1]) == "r":
+			temp_var_name = split_var(i[1])[0]
+			code += ["mov %{}, %eax".format(temp_vars[temp_var_name]["ro"])]
+			remove_used_temp_var_in_reg(temp_var_name)
+
+	elif i[0] in ["pop", "call", "mov", "add", "sub"]: # push and pop
 		code += [line[:-1]]
 
-	elif i[0][:3]=="int":
+	elif i[0] == "push":
+		if get_type(i[1]) == "c":
+			code += ["push {}".format(i[1])]
+		elif get_type(i[1]) == "m":
+			offset = 0
+			if "var" in i[1]:
+				offset = int(split_var(i[1])[1])
+			elif "tmp#" in i[1]:
+				temp_var_name = split_var(i[1])[0]
+				offset = temp_vars[temp_var_name]["so"]
+			code += ["push {}(%ebp)".format(-offset)]
+		elif get_type(i[1]) == "r":
+			temp_var_name = split_var(i[1])[0]
+			code += ["push %{}".format(temp_vars[temp_var_name]["ro"])]
+			remove_used_temp_var_in_reg(temp_var_name)
+		elif get_type(i[1]) == -1:
+			code += ["push {}".format(i[1])]
+
+	elif i[0] == "=":
+		if (get_type(i[1]), get_type(i[2])) == ("m", "c"): #m=c
+			var_offset = int(split_var(i[1])[1])
+			code += ["mov {}, {}(%ebp)".format(i[2], -var_offset)]
+		elif (get_type(i[1]), get_type(i[2])) == ("m", "r"): #m=r
+			temp_var_name = split_var(i[2])[0]
+			var_offset = int(split_var(i[1])[1])
+			code += ["mov %{}, {}(%ebp)".format(temp_vars[temp_var_name]["ro"], -var_offset)]
+			remove_used_temp_var_in_reg(temp_var_name)
+		elif (get_type(i[1]), get_type(i[2])) == ("m", "m"): #m=m
+			var_width = int(split_var(i[2])[-1])
+			var_offset_to = int(split_var(i[1])[1])
+			var_offset_from = int(split_var(i[2])[1])
+			reg=get_reg("#", var_width)
+			code += ["mov {}(%ebp), %{}".format(-var_offset_from, reg)]
+			code += ["mov %{}, {}(%ebp)".format(reg, -var_offset_to)]
+			remove_used_temp_var_in_reg("#")
+
+	elif i[0][:3]=="int" or i[0][:4]=="bool":
 		op = op_dic[i[0][3]]
 		left_temp_var_name = split_var(i[1])[0]
 		width = split_var(i[1])[1]
 		# print(left_temp_var_name, width)
-		if (get_type(i[2]),get_type(i[3])) == ("c","c"): #c+c
+		if (get_type(i[2]), get_type(i[3])) == ("c", "c"): #c+c
 			const1 = i[2]
 			const2 = i[3]
 			new_reg=get_reg(left_temp_var_name, width)
 			code += ["mov {}, %{}".format(const1, new_reg)]
 			code += ["{} {}, %{}".format(op, const2, new_reg)]
 
-		elif (get_type(i[2]),get_type(i[3])) == ("r","c"): #r+c
+		elif (get_type(i[2]), get_type(i[3])) == ("r", "c"): #r+c
 			temp_var_name = split_var(i[2])[0]
 			const = i[3]
 			code += ["{} {}, %{}".format(op, const, temp_vars[temp_var_name]["ro"])]
 			transfer_reg_to_left(temp_var_name, left_temp_var_name, width)
 
-		elif (get_type(i[2]),get_type(i[3])) == ("c","r"): #c+r
+		elif (get_type(i[2]), get_type(i[3])) == ("c", "r"): #c+r
 			temp_var_name = split_var(i[3])[0]
 			const = i[2]
 			new_reg=get_reg(left_temp_var_name, width)
@@ -147,7 +199,7 @@ for line in ir:
 			code+= ["{} %{}, %{}".format(op, temp_vars[temp_var_name]["ro"], new_reg)]
 			remove_used_temp_var_in_reg(temp_var_name)
 
-		elif (get_type(i[2]),get_type(i[3])) == ("m","c"): #m+c
+		elif (get_type(i[2]), get_type(i[3])) == ("m", "c"): #m+c
 			if "tmp#" in i[2]:
 				temp_var_name = split_var(i[2])[0]
 				const = i[3]
@@ -162,7 +214,7 @@ for line in ir:
 				code += ["mov {}(%ebp), %{}".format(-var_offset, new_reg)]
 				code += ["{} {}, %{}".format(op, const, new_reg)]
 
-		elif (get_type(i[2]),get_type(i[3])) == ("c","m"): #c+m
+		elif (get_type(i[2]), get_type(i[3])) == ("c", "m"): #c+m
 			if "tmp#" in i[3]:
 				temp_var_name = split_var(i[3])[0]
 				const = i[2]
@@ -177,7 +229,7 @@ for line in ir:
 				code += ["mov {}, %{}".format(const, new_reg)]
 				code += ["{} {}(%ebp), %{}".format(op, -var_offset, new_reg)]
 
-		elif (get_type(i[2]),get_type(i[3])) == ("r","r"): #r+r
+		elif (get_type(i[2]), get_type(i[3])) == ("r", "r"): #r+r
 			if "tmp#" in i[2] and "tmp#" in i[3]:
 				temp_var_name1 = split_var(i[2])[0]
 				temp_var_name2 = split_var(i[3])[0]
@@ -185,7 +237,7 @@ for line in ir:
 				transfer_reg_to_left(temp_var_name1, left_temp_var_name, width)
 				remove_used_temp_var_in_reg(temp_var_name2)
 
-		elif (get_type(i[2]),get_type(i[3])) == ("r","m"): #r+m
+		elif (get_type(i[2]), get_type(i[3])) == ("r", "m"): #r+m
 			if "tmp#" in i[2] and "tmp#" in i[3]:
 				temp_var_name1 = split_var(i[2])[0]
 				temp_var_name2 = split_var(i[3])[0]
@@ -198,7 +250,7 @@ for line in ir:
 				code += ["{} {}(%ebp), %{}".format(op, -var_offset, temp_vars[temp_var_name1]["ro"])]
 				transfer_reg_to_left(temp_var_name1, left_temp_var_name, width)
 
-		elif (get_type(i[2]),get_type(i[3])) == ("m","r"): #m+r
+		elif (get_type(i[2]), get_type(i[3])) == ("m", "r"): #m+r
 			if "tmp#" in i[2] and "tmp#" in i[3]:
 				temp_var_name1 = split_var(i[2])[0]
 				temp_var_name2 = split_var(i[3])[0]
@@ -215,7 +267,7 @@ for line in ir:
 				code += ["{} %{}, %{}".format(op, temp_vars[temp_var_name1]["ro"], new_reg)]
 				remove_used_temp_var_in_reg(temp_var_name1)
 
-		elif (get_type(i[2]),get_type(i[3])) == ("m","m"): #m+m
+		elif (get_type(i[2]), get_type(i[3])) == ("m", "m"): #m+m
 			loc1=""
 			loc2=""
 			new_reg=get_reg(left_temp_var_name, width)
